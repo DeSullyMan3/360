b55ed545bec96ae56aabb149fdec6dd047f44d2f
DeSullyMan3 <127885070+DeSullyMan3@users.noreply.github.com>
2025-06-26 11:20:32 +0200

    Update and rename main.js to script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('submitBtn');
+  btn.addEventListener('click', async () => {
+    const password = document.getElementById('pw').value;
+    const encoder = new TextEncoder();
+    const data = encoder.encode(password);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+
+    const correctHash = 'f15f1c7b9ce93806b400fc20c273c99bc18201c16b85c00f7b93eb5b594e4a76';
+    if (hashHex === correctHash) {
+      window.location.href = "menu.html";
+    } else {
+      document.getElementById("error").style.display = "block";
+    }
+  });
+});
