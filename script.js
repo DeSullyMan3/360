173cbede9bd84b36250bb58e582e71f16ca433d3
DeSullyMan3 <127885070+DeSullyMan3@users.noreply.github.com>
2025-06-26 11:24:53 +0200

    awa

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const btn = document.getElementById('submitBtn');
-  btn.addEventListener('click', async () => {
-    const password = document.getElementById('pw').value;
-    const encoder = new TextEncoder();
-    const data = encoder.encode(password);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  const errorMsg = document.getElementById('error');
 
-    const correctHash = 'f15f1c7b9ce93806b400fc20c273c99bc18201c16b85c00f7b93eb5b594e4a76';
-    if (hashHex === correctHash) {
-      window.location.href = "menu.html";
+  btn.addEventListener('click', () => {
+    const pw = document.getElementById('pw').value;
+    if (pw === 'appelsap') {
+      // Correct – go to menu.html
+      window.location.href = 'menu.html';
     } else {
-      document.getElementById("error").style.display = "block";
+      errorMsg.style.display = 'block';
     }
   });
 });
